d1ec671697b9717f751204847f045b0a7406fc27
MikAndTon <88402040+MikAndTon@users.noreply.github.com>
2021-08-03 22:17:52 +0300

    Update mainmenu.java

--- mainmenu.java
+++ mainmenu.java
@@ -1,11 +1,7 @@
 package Menu;
-
 public class mainmenu {
     public static void main(String[] args)throws Exception {
-
             menu.main();
             System.out.println("\nЗавершаю работу...");
-
-
     }
 }
